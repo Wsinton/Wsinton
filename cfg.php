586fd93e7f0b1c48b2d2e2d39678b96e9623e2c6
Wsinton <91981320+Wsinton@users.noreply.github.com>
2021-10-23 22:57:29 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,49 @@
+<?php
+
+$useragent = "Mozilla/5.0 (Linux; Android 10; RMX2030) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.152 Mobile Safari/537.36";
+
+$cookie = "_ga=GA1.2.2071402799.1634144822; _gid=GA1.2.292547092.1634913690; _data_html=158-1; PHPSESSID=bvbcd8eruojht0v2caq6k1drkt; _gat_gtag_UA_172139373_2=1";
+
+//Autowd
+//minimum 0.0000005 LTC / 50 Litoshi
+$withdraw = "0.0000005";
+
+//Timer collect profit
+$tmr = "30";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Copyright©2021 - YzZz Tv Official YouTube Channel
+//Do not sell my script!!!
+//Do not put in password or shortlink to my script!!!
+//Report to YouTube if you do that!!!
